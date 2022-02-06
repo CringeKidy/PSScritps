8a4adcfab70d691b42b2c49340fc39bf95988712
CringeKidy <justacecornelder15@gmail.com>
2022-02-06 19:59:34 +1300

    Update to scripts

--- New-Machine/new-machine.ps1
+++ New-Machine/new-machine.ps1
@@ -1,55 +1,58 @@
 #This If statment is the first thing that gets called when script is ran
 #It makes sure that the currnet ps session is running as administrator
 #and if not then it will open a new PS Session with Administrator perms
-if ($(new-object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator) -or $($Elevated -eq $True)) {
-  Start-Process powershell -Verb runAs -ArgumentList "-NoExit";
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+Set-ExecutionPolicy Unrestricted
+
+
+$profileFile = "Wifi.xml"
+$SSID = 'www.miltech.co.nz'
+$PW = 'bEtt3rW!f1'
+
+if(Test-Path($profileFile)){
+  Remove-Item($profileFile)
 }
 
-#Connecting to WIFI
-# Fill in mandatory details for the WiFi network
-$WirelessNetworkSSID = 'www.miltech.co.nz'
-$WirelessNetworkPassword = 'bEtterW!f1'
-$Authentication = 'WPA2PSK' # Could be WPA2
-$Encryption = 'AES'
-
-# Create the WiFi profile, set the profile to auto connect
-$WirelessProfile = @'
-<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1"&gt;
-	<name>{0}</name>
-	<SSIDConfig>
-		<SSID>
-			<name>{0}</name>
-		</SSID>
-	</SSIDConfig>
-	<connectionType>ESS</connectionType>
-	<connectionMode>auto</connectionMode>
-	<MSM>
-		<security>
-			<authEncryption>
-				<authentication>{2}</authentication>
-				<encryption>{3}</encryption>
-				<useOneX>false</useOneX>
-			</authEncryption>
-			<sharedKey>
-				<keyType>passPhrase</keyType>
-				<protected>false</protected>
-				<keyMaterial>{1}</keyMaterial>
-			</sharedKey>
-		</security>
-	</MSM>
+$xmlfile="<?xml version=""1.0""?>
+<WLANProfile xmlns=""http://www.microsoft.com/networking/WLAN/profile/v1"">
+    <name>$SSID</name>
+    <SSIDConfig>
+        <SSID>
+            <name>$SSID</name>
+        </SSID>
+    </SSIDConfig>
+    <connectionType>ESS</connectionType>
+    <connectionMode>auto</connectionMode>
+    <MSM>
+        <security>
+            <authEncryption>
+                <authentication>WPA2PSK</authentication>
+                <encryption>AES</encryption>
+                <useOneX>false</useOneX>
+            </authEncryption>
+            <sharedKey>
+                <keyType>passPhrase</keyType>
+                <protected>false</protected>
+                <keyMaterial>$PW</keyMaterial>
+            </sharedKey>
+        </security>
+    </MSM>
 </WLANProfile>
-'@ -f $WirelessNetworkSSID, $WirelessNetworkPassword, $Authentication, $Encryption
+"
 
-# Create the XML file locally s
-$random = Get-Random –Minimum 1111 –Maximum 99999999
-$tempProfileXML = "$env:TEMP\tempProfile$random.xml"
-$WirelessProfile | Out-File $tempProfileXML
+$xmlfile > ($profileFile)
+netsh wlan add profile filename="$($profileFile)"
+netsh wlan show profiles $SSID key=clear
+netsh wlan connect name=$SSID
 
-# Add the WiFi profile and connect
-Start-Process netsh ('wlan add profile filename={0}' -f $tempProfileXML)
-
-# Connect to the WiFi network – only if you need to
-Start-Process netsh ('wlan connect name="{0}"' -f $WirelessNetworkSSID)
+while (!(test-connection 1.1.1.1 -Count 1 -Quiet)) {
+  Read-Host "There seems to be no internet connection please fix this before carrying on"
+}
 
 Write-Host "This may take a while"
 $urls = @(
@@ -75,3 +78,5 @@ Import-Module PSWindowsUpdate
 
 #Gets and install the updates that it finds
 Get-WindowsUpdate -Download -Install -ForceInstall -ForceDownload -AcceptAll -AutoReboot
+
+Read-Host "Thanks for using this script :)"
